ec47201797a8e7fc9b1c9af80a7d5d7c578d00ab
Unknown <Marcel.Reckmann@yahoo.de>
2019-01-02 20:37:14 +0100

    Fixed Test Error

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,18 +1,20 @@
-/*package sfgpetclinic;
+package sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SuppressWarnings("MagicConstant")
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SfgPetClinicApplication.class)
 public class SfgPetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
     }
 
-}*/
+}
 
